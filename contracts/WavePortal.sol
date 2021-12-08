6b24cec7be5a5aa49c36aee9b8f844cb58471ba6
grzegorz.kubiak <grzegorz.kubiak@tsh.io>
2021-12-08 19:26:37 +0100

    Add storing all waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,18 +7,34 @@ import "hardhat/console.sol";
 contract WavePortal {
   uint256 totalWaves;
 
+  event NewWave(address indexed from, uint256 timestamp, string message);
+
+  struct Wave {
+    address waver;
+    string message;
+    uint256 timestamp;
+  }
+
+  Wave[] waves;
+
   constructor(){
     console.log("Hello from the smart contract");
   }
 
-  function wave() public {
+  function wave(string memory _message) public {
     totalWaves += 1;
-    console.log("%s has waved!", msg.sender);
+    console.log("%s has waved with message %s", msg.sender, _message);
+    waves.push(Wave(msg.sender, _message, block.timestamp));
+    emit NewWave(msg.sender, block.timestamp, _message);
   }
 
   function getTotalWaves() public view returns (uint256) {
     console.log("We have %d total waves!", totalWaves);
     return totalWaves;
+  }
+
+  function getAllWaves() public view returns (Wave[] memory) {
+    return waves;
   }
 }
 
