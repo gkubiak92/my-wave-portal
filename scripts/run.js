23e7d8fda7791a246e3591b23f87985d834a7ce9
grzegorz.kubiak <grzegorz.kubiak@tsh.io>
2021-12-09 18:17:54 +0100

    Add random winner functionallity

--- scripts/run.js
+++ scripts/run.js
@@ -9,8 +9,11 @@ const main = async () => {
   let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
   console.log('Contract balance: ', hre.ethers.utils.formatEther(contractBalance));
 
-  let waveTxn = await waveContract.wave('Message!');
+  let waveTxn = await waveContract.wave('This is wave #1!');
   await waveTxn.wait();
+
+  let waveTxn2 = await waveContract.wave('This is wave #2!');
+  await waveTxn2.wait();
 
   contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
   console.log(
